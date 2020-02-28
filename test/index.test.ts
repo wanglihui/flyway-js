96e346fe8834ffc0cf76edc61943351f1570c942
shiyunlai <shiyunlai@gmail.com>
2020-02-28 17:58:13 +0800

    对baseline进行测试

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,6 +3,13 @@ import FlywayJs from "../index";
 describe("FlywayJs", () => {
 
     it("#run should be ok", async() => {
-        return new FlywayJs(process.env.DB_URL, process.cwd()+'/test/sql', true).run();
+        //如果要关闭文件hash校验需要传递 options参数
+        let options = {
+            allowHashNotMatch: false,
+            // baseline: "V01__baseline.sql"
+            // baseline: ""
+             baseline: "V37__handed.sql"
+        }
+        return new FlywayJs(process.env.DB_URL, process.cwd()+'/test/sql', false, options).run();
     })
 })

edbb9f60db3039266b79a3f5bf07ef40d92a89c0
shiyunlai <shiyunlai@gmail.com>
2020-03-02 11:31:42 +0800

    完善单元测试

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,11 @@ import FlywayJs from "../index";
 
 describe("FlywayJs", () => {
 
+    before(()=>{
+        process.env.DB_URL = process.env.DB_URL || "mysql://127.0.0.1:3306/itp-flyway?user=root&password=root123098"
+        console.log('DB_URL is:', process.env.DB_URL )
+    })
+
     it("#run should be ok", async() => {
         //如果要关闭文件hash校验需要传递 options参数
         let options = {
@@ -10,6 +15,6 @@ describe("FlywayJs", () => {
             // baseline: ""
              baseline: "V37__handed.sql"
         }
-        return new FlywayJs(process.env.DB_URL, process.cwd()+'/test/sql', false, options).run();
+        return new FlywayJs(process.env.DB_URL, process.cwd()+'/test/sql', true, options).run();
     })
 })
